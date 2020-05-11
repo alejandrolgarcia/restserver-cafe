bd7b2f74324ee3e7d2da4991b706a8cb67aa442e
Luis Garcia <lalejandro.1988@gmail.com>
2020-05-10 21:05:47 -0600

    RestServer - usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+const app = express();
+
+// GET
+app.get('/usuario', function(req, res) {
+
+    // especificar limites de busqueda (paginación)
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            // Si error
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err,
+                });
+            }
+
+            // si OK
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    contar: conteo,
+                });
+            });
+        });
+});
+
+// Post
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role,
+    });
+
+    // Grabar en mongo
+    usuario.save((err, usuarioDB) => {
+
+        // Si error
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        // usuarioDB.password = null;
+
+        // Si es ok
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    });
+});
+
+// Put
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']); // retorna solo los valores que queremos del objeto
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        // Si error
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err,
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+// Delete
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    let cambiarEstado = {
+        estado: false
+    }
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+    Usuario.findByIdAndUpdate(id, cambiarEstado, { new: true }, (err, usuarioBorrado) => {
+        // Si error
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err,
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado',
+                },
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado,
+        });
+    });
+
+
+
+});
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('./config/config');
+
+const mongoose = require('mongoose');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
+
+// Conexión a Mongo
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE...');
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', 3000);
+});
